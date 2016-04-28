063e257eed6ec1420249b860d13557c49203449a
Chris Gianelloni <chris@cask.co>
2016-04-28 12:05:06 -0400

    CDAP-3957 Support custom CDAP repositories

--- package/scripts/params.py
+++ package/scripts/params.py
@@ -40,12 +40,14 @@ if distribution.startswith('centos') or distribution.startswith('redhat'):
     package_mgr = 'yum'
     key_cmd = "rpm --import %s/pubkey.gpg" % (files_dir)
     cache_cmd = 'yum makecache'
+    repo_url = config['configurations']['cdap-env']['yum_repo_url']
 else:
     os_repo_dir = '/etc/apt/sources.list.d/'
     repo_file = 'cdap-3.4.list'
     package_mgr = 'apt-get'
     key_cmd = "apt-key add %s/pubkey.gpg" % (files_dir)
     cache_cmd = 'apt-get update'
+    repo_url = config['configurations']['cdap-env']['apt_repo_url']
 
 cdap_user = config['configurations']['cdap-env']['cdap_user']
 log_dir = config['configurations']['cdap-env']['cdap_log_dir']
