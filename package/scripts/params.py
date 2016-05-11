60b9dd18244544746e7e5b7c561bb4cd07c98bd1
Chris Gianelloni <chris@cask.co>
2016-05-11 18:07:11 -0400

    Set router.bind.port to 11015

--- package/scripts/params.py
+++ package/scripts/params.py
@@ -100,12 +100,4 @@ router_hosts = config['clusterHostInfo']['cdap_router_hosts']
 router_hosts.sort()
 cdap_router_host = router_hosts[0]
 
-# Get some of our hosts
-hive_metastore_host = config['clusterHostInfo']['hive_metastore_host']
-hive_server_host = config['clusterHostInfo']['hive_server_host']
-if len(hive_server_host) > 0 and hive_server_host == cdap_router_host:
-    cdap_router_port = '11015'
-else:
-    cdap_router_port = '10000'
-
 # TODO: cdap_auth_server_hosts cdap_ui_hosts
